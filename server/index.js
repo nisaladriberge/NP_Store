d041d761916469e78281b266b2d10fdd17cdd879
Pamod1478 <pamodsachintha2003@gmail.com>
2025-03-16 10:31:22 +0530

    made some changes

--- server/index.js
+++ server/index.js
@@ -1,31 +1,35 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config()
-import cookieParser from 'cookie-parser'
-import morgan from 'morgan'
-import helmet from 'helmet'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
 
-const app = express()
-app.use(cors({
-    Credentials : true,
-    origin : process.env.FRONTEND_URL
-}))
-app.use(express.json())
-app.use(cookieParser())
-app.use(morgan())
-app.use(helmet({
-    crossOrginResourcePolicy : false
-}))
+const app = express();
+app.use(
+  cors({
+    Credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(
+  helmet({
+    crossOrginResourcePolicy: false,
+  })
+);
 
-const PORT = 8080 || process.env.PORT
+const PORT = 8080 || process.env.PORT;
 
-app.get("/",(request,response)=>{
-    ///server to client
-    response.json({
-        message : "Server is running " + PORT
-    })
-})
-app.listen(PORT,()=>{
-    console.log("Server is running",PORT)
-})
+app.get("/", (request, response) => {
+  ///server to client
+  response.json({
+    message: "Server is running 1234 " + PORT,
+  });
+});
+app.listen(PORT, () => {
+  console.log("Server is running", PORT);
+});
